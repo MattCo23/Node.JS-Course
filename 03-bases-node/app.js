b48d56992ec97bb81501f060e38850daeca9473d
MattCo23 <mateo_2992@hotmail.com>
2021-03-15 16:44:09 +0100

    Node basics

---
+++ 03-bases-node/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.clear();
+
+console.log('==========================');
+console.log('====    Tabla del 5   ====');
+console.log('==========================');
+const base = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${base} x ${i} = ${5 * i}`);
+}
