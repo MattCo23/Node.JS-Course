841f08bc2b86bcc2836acbdc3e9e58d17219ed28
MattCo23 <mateo_2992@hotmail.com>
2021-03-17 18:50:20 +0100

    Independent config of yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,28 +1,9 @@
 'use strict';
 
 const { crearArchivo } = require('./helpers/multiplicar');
-
-const argv = require('yargs')
-  .option('b', {
-    alias: 'base',
-    type: 'number',
-    demandOption: true,
-  })
-  .option('l', {
-    alias: 'listar',
-    type: 'boolean',
-    default: false,
-  })
-  .check((argv, options) => {
-    if (isNaN(argv.b)) {
-      throw 'La base tiene que ser un número';
-    }
-    return true;
-  }).argv;
+const argv = require('./config/yargs');
 
 console.clear();
-
-console.log(argv);
 
 // const [, , arg3 = 'base=5'] = process.argv;
 // const [, base = 5] = arg3.split('=');
