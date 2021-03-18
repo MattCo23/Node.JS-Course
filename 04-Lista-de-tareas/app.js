8263220db35e4d3a6b98a39b0d69c5a5f9e50932
MattCo23 <mateo_2992@hotmail.com>
2021-03-18 15:51:10 +0100

    Repeat menu loop

--- 04-Lista-de-tareas/app.js
+++ 04-Lista-de-tareas/app.js
@@ -4,7 +4,15 @@ const { mostrarMenu, pausa } = require('./helpers/mensajes');
 console.clear();
 
 const main = async () => {
-  mostrarMenu();
+  let opt = '';
+
+  do {
+    opt = await mostrarMenu();
+    console.log({ opt });
+
+    if (opt !== '0') await pausa();
+  } while (opt !== '0');
+
   //   pausa();
 };
 
