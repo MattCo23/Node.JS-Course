3d0b5d66e19c558ead48bbcf41646853074f6912
MattCo23 <mateo_2992@hotmail.com>
2021-03-18 15:25:48 +0100

    stdin and stdout

--- 04-Lista-de-tareas/app.js
+++ 04-Lista-de-tareas/app.js
@@ -1,8 +1,11 @@
 require('colors');
 
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+console.clear();
+
 const main = async () => {
-  console.clear();
-  console.log('Hola Mundo');
+  mostrarMenu();
+  //   pausa();
 };
 
 main();
