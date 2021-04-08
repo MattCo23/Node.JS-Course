bd6919fcedc4d112b5efffadbe9fea93147fe33b
MattCo23 <mateo_2992@hotmail.com>
2021-04-08 21:18:28 +0200

    Create town input

--- 05-Weather-App/index.js
+++ 05-Weather-App/index.js
@@ -1,11 +1,38 @@
 const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 const main = async () => {
+  const busquedas = new Busquedas();
   let opt = '';
 
   do {
-    opt = inquirerMenu();
-  } while (opt === 0);
+    opt = await inquirerMenu();
+    switch (opt) {
+      case 1:
+        // Mostrar mensaje
+        const lugar = await leerInput('Ciudad: ');
+        console.log(lugar);
+
+        // Buscar los lugares
+
+        // Seleccionar el lugar
+
+        // Clima
+
+        //MOstrar resultados
+        console.log('\nInformación de la ciudad\n'.green);
+        console.log('Ciudad:');
+        console.log('Lat:');
+        console.log('Lon:');
+        console.log('Temperatura:');
+        console.log('Mínima:');
+        console.log('Máxima:');
+
+        break;
+    }
+
+    if (opt !== 0) await pausa();
+  } while (opt !== 0);
 };
 
 main();
