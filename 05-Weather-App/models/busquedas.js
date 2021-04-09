30784bfc8330193ad3e4971a92c26e768ed654b2
MattCo23 <mateo_2992@hotmail.com>
2021-04-09 16:37:00 +0200

    Create first endpoint with mapbox

--- 05-Weather-App/models/busquedas.js
+++ 05-Weather-App/models/busquedas.js
@@ -10,7 +10,9 @@ class Busquedas {
   async ciudad(lugar = '') {
     try {
       // peticion http
-      const resp = await axios.get('https://reqres.in/api/users?page=2');
+      const resp = await axios.get(
+        'https://api.mapbox.com/geocoding/v5/mapbox.places/ottawa.json?access_token=pk.eyJ1IjoibWF0dGNvMjMiLCJhIjoiY2tuYWVoOG5jMG16eDJwbXJpMTl5bGhrMiJ9.CsxND9sZzMSTVR6vqK5lcQ&limit=5&language=es'
+      );
       console.log(resp.data);
 
       return [];
