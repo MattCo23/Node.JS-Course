fac1aebf423c6ea0ce94b1d4f3bdc0f17e362bae
MattCo23 <mateo_2992@hotmail.com>
2021-04-09 15:50:59 +0200

    Made first HTTP req with Axios

--- 05-Weather-App/models/busquedas.js
+++ 05-Weather-App/models/busquedas.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 class Busquedas {
   historial = ['Tegucigalpa', 'Madrid', 'San José'];
 
@@ -6,9 +8,15 @@ class Busquedas {
   }
 
   async ciudad(lugar = '') {
-    // peticion http
+    try {
+      // peticion http
+      const resp = await axios.get('https://reqres.in/api/users?page=2');
+      console.log(resp.data);
 
-    console.log(lugar);
+      return [];
+    } catch (error) {
+      return [];
+    }
 
     return []; // retornar lugares
   }
