f7e3abdc86336b464ded0b434d3faccc581f7bd0
MattCo23 <mateo_2992@hotmail.com>
2021-04-13 16:00:31 +0200

    Created search history

--- 05-Weather-App/index.js
+++ 05-Weather-App/index.js
@@ -24,21 +24,34 @@ const main = async () => {
 
         // Seleccionar el lugar
         const id = await listarLugares(lugares);
+        if (id === '0') continue;
+
         const lugarSel = lugares.find((l) => l.id === id);
+
+        // Guardar en DB
+        busquedas.agregarHistorial(lugarSel.nombre);
 
         // Clima
         const tempSel = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
 
         //MOstrar resultados
+        console.clear();
         console.log('\nInformación de la ciudad\n'.green);
-        console.log('Ciudad:', lugarSel.nombre);
+        console.log('Ciudad:', lugarSel.nombre.green);
         console.log('Lat:', lugarSel.lat);
         console.log('Lon:', lugarSel.lng);
         console.log('Temperatura:', tempSel.temp);
         console.log('Mínima:', tempSel.min);
         console.log('Máxima:', tempSel.max);
-        console.log('Descripción:', tempSel.desc);
+        console.log('Descripción:', tempSel.desc.green);
 
+        break;
+
+      case 2:
+        busquedas.historialCapitalizado.forEach((lugar, i) => {
+          const idx = `${i + 1}.`.green;
+          console.log(`${idx} ${lugar}`);
+        });
         break;
     }
 
--- 05-Weather-App/models/busquedas.js
+++ 05-Weather-App/models/busquedas.js
@@ -1,10 +1,22 @@
+const fs = require('fs');
+
 const axios = require('axios');
 
 class Busquedas {
-  historial = ['Tegucigalpa', 'Madrid', 'San José'];
+  historial = [];
+  dbPath = './db/database.json';
 
   constructor() {
-    // TODO: leer DB si existe
+    this.leerDB();
+  }
+
+  get historialCapitalizado() {
+    return this.historial.map((lugar) => {
+      let palabras = lugar.split(' ');
+      palabras = palabras.map((p) => p[0].toUpperCase() + p.substring(1));
+
+      return palabras.join(' ');
+    });
   }
 
   get paramsMapbox() {
@@ -47,13 +59,7 @@ class Busquedas {
     try {
       const instance = axios.create({
         baseURL: `https://api.openweathermap.org/data/2.5/weather`,
-        params: {
-          lat,
-          lon,
-          appid: process.env.OPEN_WEATHER,
-          units: 'metric',
-          lang: 'es',
-        },
+        params: { ...this.paramsOpenWeather, lat, lon },
       });
 
       const resp = await instance.get();
@@ -67,6 +73,35 @@ class Busquedas {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  agregarHistorial(lugar = '') {
+    // Prevenir duplicados
+    if (this.historial.includes(lugar.toLocaleLowerCase())) {
+      return;
+    }
+    this.historial = this.historial.splice(0, 4);
+
+    this.historial.unshift(lugar.toLocaleLowerCase());
+
+    // Grabar en DB
+    this.guardarDB();
+  }
+
+  guardarDB() {
+    const payload = {
+      historial: this.historial,
+    };
+
+    fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+  }
+
+  leerDB() {
+    if (!fs.existsSync(this.dbPath)) return;
+
+    const info = fs.readFileSync(this.dbPath, { encoding: 'utf-8' });
+    const data = JSON.parse(info);
+    this.historial = data.historial;
   }
 }
 
