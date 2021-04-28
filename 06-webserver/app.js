2d086344b9373bbc0d64a8aba496ca6d98f663fd
MattCo23 <mateo_2992@hotmail.com>
2021-04-28 13:20:42 +0200

    Creating a server with Express

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,10 +1,19 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-http
-  .createServer((req, res) => {
-    res.write('Hola Mundo');
-    res.end();
-  })
-  .listen(8080);
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
 
-console.log('Escuchando el puerto', 8080);
+app.get('/hola-mundo', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('*', (req, res) => {
+  res.send('404 | Page not Found');
+});
+
+app.listen(port, () => {
+  console.log('Escuchando al puerto 8080');
+});
